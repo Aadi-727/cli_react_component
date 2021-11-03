ac48a91dd39f11d4a7a588e3ca0af22a2aba1f06
Zoyo <adithyanas1@gmail.com>
2021-11-03 20:47:39 +0530

    new day

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const generateComponent = require("./utils/createComponent");
 app.version("1.0.0").description("React component generator CLI");
 
 app
-	.option(
-		"-n, --name <name> ",
-		"creates the component in a folder",
-		"Component"
-	)
 	.option("-f, --folder", "creates the component in a folder")
 	.option("-c, --stylesheet, --css", "include a stylesheet file")
 	.option("-i, --importcss", "import the css file in jsx file")
 	.parse();
 
-const { name, folder, stylesheet, importcss } = app.opts();
+const name = process.argv[2];
+console.log(process.argv);
+const { folder, stylesheet, importcss } = app.opts();
 console.log(name, folder, stylesheet);
 if (name) {
 	generateComponent(name, folder, stylesheet, importcss);
